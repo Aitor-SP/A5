a8a4167b47b744e0385e6b1c2959bf1007c41d2b
aitorsp-elpuig <aitorsp@elpuig.xeill.net>
2020-12-03 19:21:32 +0100

    Initial commit

---
+++ src/com/company/Cifrar.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+import java.security.*;
+import java.util.Arrays;
+
+public class Cifrar {
+
+    // 1.1.i
+    public static KeyPair randomGenerate(int len) {
+        KeyPair keys = null;
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(len);
+            keys = keyGen.genKeyPair();
+        } catch (Exception ex) {
+            System.err.println("Generador no disponible.");
+        }
+        return keys;
+    }
+
+    public static byte[] encryptData(byte[] data, PublicKey pub) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.ENCRYPT_MODE, pub);
+            encryptedData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return encryptedData;
+    }
+
+    public static byte[] decryptData(byte[] data, PrivateKey pub) {
+        byte[] decryptData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.DECRYPT_MODE, pub);
+            decryptData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return decryptData;
+    }
+
+    public static KeyStore loadKeyStore(String ksFile, String ksPwd) throws Exception {
+        KeyStore ks = KeyStore.getInstance("JKS");
+        File f = new File (ksFile);
+        if (f.isFile()) {
+            FileInputStream in = new FileInputStream (f);
+            ks.load(in, ksPwd.toCharArray());
+        }
+        return ks;
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,53 @@
+package com.company;
+
+import java.security.Key;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        String keystorePath = "/home/dam2a/.keystore";
+        String keystorePassword = "123Dam456";
+
+        Scanner input = new Scanner(System.in);
+
+        // 1
+        KeyPair keypair = Cifrar.randomGenerate(1024);
+        PublicKey publicKey = keypair.getPublic();
+        PrivateKey privateKey = keypair.getPrivate();
+
+
+        /*/System.out.print("Introduzca el texto a cifrar: ");
+        String texto = input.nextLine();
+        byte[] prueba = texto.getBytes();
+
+        byte[] cifrar = Cifrar.encryptData(prueba, publicKey);
+        System.out.println(cifrar);
+
+        byte[] descifrar = Cifrar.decryptData(cifrar, privateKey);
+        System.out.println(new String(descifrar));/*/
+
+        // 2
+        // 2.1
+        System.out.println("Tipo de Keystore: "+Cifrar.loadKeyStore(keystorePath,keystorePassword).getType());
+
+        // 2.2
+        System.out.println("Tamaño de la Keystore: "+Cifrar.loadKeyStore(keystorePath,keystorePassword).size());
+
+        // 2.3
+        System.out.println("Alias de la Keystore: "+ Cifrar.loadKeyStore(keystorePath,keystorePassword).aliases().nextElement());
+
+        // 2.4
+        System.out.println("Certificado de la clave: "+Cifrar.loadKeyStore(keystorePath,keystorePassword).getCertificate("lamevaclaum9"));
+
+        // 2.5
+        System.out.println("Algoritmo cifrado de la clave: "+Cifrar.loadKeyStore(keystorePath,keystorePassword).getCertificate("lamevaclaum9").getPublicKey().getAlgorithm());
+
+        // 2.ii
+
+    }
+}
